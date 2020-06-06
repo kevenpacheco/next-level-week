498f0a5e52b590abbea97cc0237cf86709ca7651
Keven Pacheco <kevenpacheco2000@gmail.com>
2020-06-06 17:48:25 -0300

    Segunda parte da tela de cadastro

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -13,21 +13,24 @@ function populateUFs() {
 populateUFs();
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city");
+    const citySelect = document.querySelector("select[name=city]");
     const stateInput = document.querySelector("input[name=state]");
 
     const ufValue = event.target.value
 
     const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value = event.target.option[indexOfSelectedState].text
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
 
     fetch(url)
     .then( res => res.json())
     .then( cities => {
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false;
