35848bb54f5c2317c04e0244b98a754f9d7154f1
Keven Pacheco <kevenpacheco2000@gmail.com>
2020-06-21 23:29:58 -0300

    Página de pesquisa

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
-const close = document.querySelector("#modal header a")
+const close = document.querySelector("#modal .header a")
 
 buttonSearch.addEventListener('click', () => {  
     modal.classList.remove("hide")
